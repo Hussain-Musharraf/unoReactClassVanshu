b50c742bde57f820a4c0a4ce29059f219ffb603f
Hussain-Musharraf <musharrafhussain4397@gmail.com>
2023-01-16 18:43:22 +0530

    formInputAdded

---
+++ form_react/src/components/NameForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+class NameForm extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            firstName:" ",
+            lastName:" "
+        }
+    }
+    render(){
+        const handleFirstNameChange=(e)=>{
+            //console.log("event is",e.target.value);
+            this.setState({
+                firstName:e.target.value
+            })
+        }
+        const handleLastNameChange=(e)=>{
+            //console.log("event is",e.target.value)
+            this.setState({
+                lastName:e.target.value
+            })
+        }
+        return(
+            <>
+            <form>
+                <input placeholder="First Name" value={this.state.firstName} onChange={handleFirstNameChange}/>
+                <input placeholder="Last Name" value={this.state.lastName} onChange={handleLastNameChange}/>
+            </form>
+            <h3>My Full Mame is {this.state.firstName} {this.state.lastName}</h3>
+            
+            </>
+        )
+    }
+}
+export default NameForm;
